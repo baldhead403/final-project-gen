8e556db2e76118e434552f8103bb224c0f5688d7
Donald Rivers <donaldrivers76@gmail.com>
2022-01-26 15:08:41 -0500

    Add files via upload

---
+++ Fantastic-Footwear/src/main/java/com/finalproject/Fantastic/Footwear/FantasticFootwearApplication.java
@@ -0,0 +1,13 @@
+package com.finalproject.Fantastic.Footwear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasticFootwearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasticFootwearApplication.class, args);
+	}
+
+}
---
+++ Fantastic-Footwear/src/test/java/com/finalproject/Fantastic/Footwear/FantasticFootwearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalproject.Fantastic.Footwear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantasticFootwearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
